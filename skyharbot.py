24386619ac29a83b569e06207d9dc4ea4ddab5b3
Marco Portillo <99373690+Americana808@users.noreply.github.com>
2023-12-31 15:40:24 -0700

    Data filtered
    added filter to only print out the correct flights needed through IF statements. (A bit slow but we are getting there!)

--- skyharbot.py
+++ skyharbot.py
@@ -1,23 +1,24 @@
-from bs4 import BeautifulSoup
-from selenium import webdriver 
+from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service as ChromeService 
-from webdriver_manager.chrome import ChromeDriverManager 
-from selenium.webdriver.common.keys import Keys
 
+url = 'https://www.skyharbor.com/flights/?AD=D&search='
 
-def get_AA_flights():
-    flightURL = 'https://www.skyharbor.com/flights/?AD=A&search='
-    driver = webdriver.Chrome()
-    driver.get(flightURL)
-    content = driver.page_source.encode('utf-8').strip()
-    flightSoup = BeautifulSoup(content,'lxml')
-    info = flightSoup.findAll("tr", class_="filter-item")
-    flightInfo = info[0].text.strip()
-    print(flightInfo)
-    
-    for infor in info:
-        print(infor.text.strip())
-    
+driver = webdriver.Chrome()
+driver.get(url)
+driver.implicitly_wait(0.5)
 
-get_AA_flights()
+element_with_data_airline = driver.find_elements(by=By.XPATH, value="//tr[@data-ad='D' and @data-airline='American Airlines']")
+
+gates = ["A22", "A24", "A25", "A26", "A27", " A28", "A29", "A30"]
+
+for element in element_with_data_airline:
+    airline = element.get_attribute("data-airline")
+    time = element.get_attribute("data-scheduled")
+    gate = element.get_attribute("data-gate")
+    estimated = element.get_attribute("data-estimated")
+    city = element.get_attribute("data-city")
+    if gate in gates:
+        if time and time[0].isdigit():
+            print("Data-airline attribute value:", city, time, gate, estimated)
+    ###print("Data-airline attribute value:", city, time, gate, estimated)
+
