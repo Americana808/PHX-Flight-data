20448a1eea905863ea898dfa1a57ce0c8543878e
Marco Portillo <99373690+Americana808@users.noreply.github.com>
2024-01-03 09:47:18 -0700

    Email and formatting
    Add functionality to send formatted emails and a requirements file to install the correct libraries.

--- skyharbot.py
+++ skyharbot.py
@@ -1,15 +1,30 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from tabulate import tabulate
+
+# EMAIL IMPORTS
+from email.message import EmailMessage
+import ssl
+import smtplib
+
+em = EmailMessage()
+email_sender = '@gmail.com'
+email_password = ''
+email_receiver = '@gmail.com'
+
 
 url = 'https://www.skyharbor.com/flights/?AD=D&search='
 
 driver = webdriver.Chrome()
 driver.get(url)
-driver.implicitly_wait(0.5)
+driver.implicitly_wait(2)
 
 element_with_data_airline = driver.find_elements(by=By.XPATH, value="//tr[@data-ad='D' and @data-airline='American Airlines']")
 
 gates = ["A22", "A24", "A25", "A26", "A27", " A28", "A29", "A30"]
+
+flightList = []
+i = 0
 
 for element in element_with_data_airline:
     airline = element.get_attribute("data-airline")
@@ -19,6 +34,24 @@ for element in element_with_data_airline:
     city = element.get_attribute("data-city")
     if gate in gates:
         if time and time[0].isdigit():
-            print("Data-airline attribute value:", city, time, gate, estimated)
-    ###print("Data-airline attribute value:", city, time, gate, estimated)
+            flightList.append([])
+            flightList[i].append(city[len(city)-4:len(city)-1])
+            flightList[i].append(time)
+            flightList[i].append(gate)
+            flightList[i].append(estimated)
+            i += 1
 
+headers = ['city', 'time', 'gate', 'actual']
+table = tabulate(flightList, headers=headers, tablefmt="table")
+# possibly format body to include the totals flights left and the when the last flgiht is.
+body = table
+
+em['From'] = email_sender
+em['To'] = email_receiver
+em['Subject'] = 'Flights as of '# !!! INSERT TIME AND DATE !!!
+em.set_content(body)
+
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+    smtp.login(email_sender, email_password)
+    smtp.sendmail(email_sender, email_receiver, em.as_string())
